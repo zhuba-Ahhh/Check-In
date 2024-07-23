85d0c0ec0625bdfa005e518353117cf3ce44afc3
zhuba-Ahhh <3477826311@qq.com>
2024-07-23 10:39:46 +0800

    feat: :sparkles: textarea 自动高度 + 去除eslint爆粗

--- src/components/Textarea.tsx
+++ src/components/Textarea.tsx
@@ -1,17 +1,35 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 interface TextareaProps {
   defaultValue?: string;
   onChange?: React.ChangeEventHandler<HTMLTextAreaElement>;
 }
 export const Textarea = ({ defaultValue, onChange }: TextareaProps) => {
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const [height, setHeight] = useState<string | number>('auto');
+
+  useEffect(() => {
+    if (textareaRef.current) {
+      setHeight('auto');
+      setHeight(textareaRef.current.scrollHeight);
+    }
+  }, [defaultValue]);
+
+  const handleChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    if (textareaRef.current) {
+      setHeight('auto');
+      setHeight(textareaRef.current.scrollHeight);
+    }
+    onChange && onChange(event); // 调用传入的 onChange 处理函数，如果有的话
+  };
   return (
     <textarea
       className="textarea textarea-bordered w-full mt-4 h-fit"
+      ref={textareaRef}
       placeholder="Bio"
       defaultValue={defaultValue}
-      onChange={onChange}
+      onChange={handleChange}
+      style={{ height }}
     />
   );
 };
